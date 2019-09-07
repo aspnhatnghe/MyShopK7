b60dc2e7fdd39f17832e665d2ebcb6a19c5c169c
Hien <hyhien@gmail.com>
2019-09-07 18:14:40 +0700

    Update library

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -37,6 +38,9 @@ namespace UI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<MyDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("MyShop")));
+
+            //services.AddAutoMapper();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
