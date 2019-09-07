9c9ab1e7f3bfbfd3fda56651b06f570a1ef81ac2
Hien <hyhien@gmail.com>
2019-09-07 19:21:38 +0700

    get product by Category

--- UI/Controllers/ProductController.cs
+++ UI/Controllers/ProductController.cs
@@ -14,9 +14,49 @@ namespace UI.Controllers
         {
             _context = db;
         }
-        public IActionResult Index()
+
+        private List<int> GetListCategoryId(int cateId)
         {
-            var data = _context.Products.ToList();
+            var result = new List<int>();
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(cateId);
+
+            while(queue.Count > 0)
+            {
+                //lấy 1 phần tử ra để xét
+                int tmp = queue.Dequeue();
+
+                //thêm ptu vừa lấy vào tập kết quả
+                result.Add(tmp);
+
+                //kiếm xem có con ko để thêm vào danh sách
+                var cateIdChild = _context.Categories.Where(p => p.ParentCategoryId == tmp).Select(p => p.CategoryId).ToList();
+                foreach(var item in cateIdChild)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+
+            return result;
+        }
+
+        public IActionResult Index(int? cateId)
+        {
+            var category = new Category {
+                CategoryName = "Tất cả"
+            };
+            var data = _context.Products.AsQueryable();
+            if (cateId.HasValue)
+            {
+                category = _context.Categories.SingleOrDefault(p => p.CategoryId == cateId);
+                //danh sách chứa các mã loại (cha + con)
+                List<int> cateIdList = GetListCategoryId(cateId.Value);
+
+                data = data.Where(p => p.CategoryId.HasValue && cateIdList.Contains(p.CategoryId.Value));
+            }
+
+            ViewBag.Category = category;
 
             return View(data);
         }
