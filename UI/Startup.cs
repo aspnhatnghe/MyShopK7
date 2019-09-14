100fa804cc25597318e5e75ee1b251ec6eb838a8
Hy <hyhien@gmail.com>
2019-09-14 20:52:51 +0700

    Update menu

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -30,7 +30,9 @@ namespace UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSession();
+            services.AddSession(opt => {
+                opt.IdleTimeout = TimeSpan.FromMinutes(15)
+            });
             //services.Configure<CookiePolicyOptions>(options =>
             //{
             //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
